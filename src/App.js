adb2246edd6760ffc9224b1c82775884c4277f41
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-19 14:26:42 +0100

    Basic layout styling.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const App = () => <div>App</div>;
+import SideBar from './components/SideBar/SideBar';
+import Chat from './components/Chat/Chat';
+import './App.scss';
+
+const App = () => (
+  <div className="app">
+    <SideBar />
+    <Chat />
+  </div>
+);
 
 export default App;
---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './SideBar.scss';
+
+function SideBar() {
+  return <div className="side-bar">SideBAr</div>;
+}
+
+export default SideBar;
