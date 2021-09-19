2797a83075468efb78cdb64680628b1480f02651
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-19 14:53:25 +0100

    Added user settings component.

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 import './SideBar.scss';
+import UserSettings from '../UserSettings/UserSettings';
 
 function SideBar() {
-  return <div className="side-bar">SideBAr</div>;
+  return (
+    <div className="side-bar">
+      <UserSettings />
+    </div>
+  );
 }
 
 export default SideBar;
