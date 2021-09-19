f437e5f6ad74498783d5b417d666ab548aeb2199
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-19 16:34:13 +0100

    Added ChatMessages component.

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './Chat.scss';
 import ChatHeader from '../ChatHeader/ChatHeader';
+import ChatMessages from '../ChatMessages/ChatMessages';
 
 function Chat() {
   return (
     <div className="chat">
       <ChatHeader />
+      <ChatMessages />
     </div>
   );
 }
