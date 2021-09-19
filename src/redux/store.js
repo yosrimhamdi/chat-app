eb05e388017c5930112699e293f8501b3ba0c444
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-19 14:02:11 +0100

    Wired redux.

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import reducers from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
