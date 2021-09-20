b045adb64ba06074b360b711b06d0aac8d6e36dc
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-20 17:16:11 +0100

    Initial firebase setup.

--- src/components/Chat/UserMessage/UserMessage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './UserMessage.scss';
-
-function UserMessage({ message }) {
-  return <div className="user-message">{message}</div>;
-}
-
-export default UserMessage;
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAfj_Z8PGpGktHz236HKIOpNXnVIEZaMEw',
+  authDomain: 'chat-app-a4b35.firebaseapp.com',
+  projectId: 'chat-app-a4b35',
+  storageBucket: 'chat-app-a4b35.appspot.com',
+  messagingSenderId: '522694988795',
+  appId: '1:522694988795:web:a670606440fd1d1bb3b15e',
+  measurementId: 'G-X3BM5DRBHW',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
