1cbc9d952e8ec5259e43ede7f8627837e458ccc1
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-20 17:32:48 +0100

    using semantic ui css.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import './App.scss';
+import 'semantic-ui-css';
 import Home from './components/Home/Home';
 import Login from './components/Auth/Login/Login';
 import Register from './components/Auth/Register/Register';
