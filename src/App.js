5f27d9d1ad7878e364b0288317126ca2fbf71be7
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-20 17:00:22 +0100

    Added Auth components and routing.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import SideBar from './components/SideBar/SideBar';
-import Chat from './components/Chat/Chat';
 import './App.scss';
+import Home from './components/Home/Home';
+import Login from './components/Auth/Login/Login';
+import Register from './components/Auth/Register/Register';
 
-const App = () => (
-  <div className="app">
-    <SideBar />
-    <Chat />
-  </div>
-);
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </Router>
+  );
+}
 
 export default App;
