58ead46c7d567e7b8979f0f73016eda9062ed40c
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-21 13:38:08 +0100

    Code refactoring.

--- src/redux/actions/onAuthStateChange.js
+++ src/redux/actions/onAuthStateChange.js
@@ -4,24 +4,13 @@ import history from '../../history';
 import { SIGN_IN, AUTH_LOADING } from '@types';
 
 const onAuthStateChange = () => dispatch => {
-  dispatch({
-    type: AUTH_LOADING,
-    payload: true,
-  });
+  dispatch({ type: AUTH_LOADING, payload: true });
 
   const auth = getAuth();
-
   onAuthStateChanged(auth, user => {
     if (user) {
-      dispatch({
-        type: SIGN_IN,
-        payload: user,
-      });
-
-      dispatch({
-        type: AUTH_LOADING,
-        payload: false,
-      });
+      dispatch({ type: SIGN_IN, payload: user });
+      dispatch({ type: AUTH_LOADING, payload: false });
 
       history.push('/');
     }
