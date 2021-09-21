dc65941d4b3eeea871a15391d3b4fde05b12c213
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-21 14:57:07 +0100

    removed some comments.

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -29,14 +29,11 @@ function UserPanel({ trySignOut, user }) {
     <Grid style={{ background: '#4c3c4c' }}>
       <Grid.Column>
         <Grid.Row style={{ padding: '1.2em', margin: 0 }}>
-          {/* App Header */}
           <Header inverted floated="left" as="h2">
             <Icon name="code" />
             <Header.Content>DevChat</Header.Content>
           </Header>
         </Grid.Row>
-
-        {/* User Dropdown  */}
         <Header style={{ padding: '0.25em' }} as="h4" inverted>
           <Dropdown trigger={<span>User</span>} options={dropdownOptions} />
         </Header>
