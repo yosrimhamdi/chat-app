7004943011c80f122b2ea3927097ec4202d1abc2
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-21 14:40:49 +0100

    Added the SidePanel component.

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import UserPanel from './UserPanel';
+import { Menu } from 'semantic-ui-react';
 
 function SidePanel() {
-  return <div>SidePanel</div>;
+  return (
+    <Menu
+      size="large"
+      inverted
+      fixed="left"
+      vertical
+      style={{ background: '#4c3c4c', fontSize: '1.2rem' }}
+    >
+      <UserPanel />
+    </Menu>
+  );
 }
 
 export default SidePanel;
