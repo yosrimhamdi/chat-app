25cb10fcc857acc075fa86d5544636ce692a6f91
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-22 14:31:20 +0100

    Added the left side color panel.

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Sidebar, Menu, Divider, Button } from 'semantic-ui-react';
 
-function ColorPanel() {
-  return <div>ColorPanel</div>;
-}
+const ColorPanel = () => {
+  return (
+    <Sidebar
+      as={Menu}
+      icon="labeled"
+      inverted
+      vertical
+      visible
+      width="very thin"
+    >
+      <Divider />
+      <Button icon="add" size="small" color="blue" />
+    </Sidebar>
+  );
+};
 
 export default ColorPanel;
