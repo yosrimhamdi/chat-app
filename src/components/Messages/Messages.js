c0def7b06eea1d160a1c8c2c356ed7c758b2aae7
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-22 14:35:17 +0100

    Added Messages components.

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Segment, Comment } from 'semantic-ui-react';
 
-function Messages() {
-  return <div>Messages</div>;
-}
+import MessagesHeader from './MessagesHeader';
+import MessageForm from './MessageForm';
+
+const Messages = () => {
+  return (
+    <React.Fragment>
+      <MessagesHeader />
+
+      <Segment>
+        <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+      </Segment>
+
+      <MessageForm />
+    </React.Fragment>
+  );
+};
 
 export default Messages;
