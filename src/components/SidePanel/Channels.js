8962331f0587bb4c5fbad3f2799cc2a689023246
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-22 16:56:14 +0100

    using onCollectionChange.

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -4,17 +4,18 @@ import { Menu, Icon } from 'semantic-ui-react';
 
 import Modal from '../Modal/Modal';
 import Channel from './Channel';
-import onChannelChange from '@actions/onChannelChange';
 import removeListener from '../../firebase/database/removeListener';
+import { FETCH_CHANNELS } from '@types';
+import onCollectionChange from '../../firebase/database/onCollectionChange';
 
-function Channels({ onChannelChange, channels }) {
+function Channels({ onCollectionChange, channels }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
-    onChannelChange();
+    onCollectionChange('channels/', FETCH_CHANNELS);
 
     return () => removeListener('channels/');
-  }, [onChannelChange]);
+  }, [onCollectionChange]);
 
   const renderedChannels = channels.all.map(channel => (
     <Channel key={channel.id} channel={channel} />
@@ -43,4 +44,4 @@ const mapStateToProps = state => ({
   channels: state.channels,
 });
 
-export default connect(mapStateToProps, { onChannelChange })(Channels);
+export default connect(mapStateToProps, { onCollectionChange })(Channels);
