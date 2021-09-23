4dc06c38e17c1ef2765f530dac61f0365a827ed4
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-23 07:08:37 +0100

    Added comments in types.js

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -1,8 +1,15 @@
+// Auth Reducer
 export const SIGN_OUT = 'SIGN_OUT';
 export const SIGN_IN = 'SIGN_IN';
+
+// Channels Reducer
 export const FETCH_CHANNELS = 'FETCH_CHANNELS';
 export const SELECT_CHANNEL = 'SELECT_CHANNEL';
+
+// Messages Reducer
 export const FETCH_MESSAGES = 'FETCH_MESSAGES';
+
+// Loading Reducers
 export const SENDING_MESSAGE = 'SENDING_MESSAGE';
 export const AUTHENTICATING = 'AUTHENTICATING';
 export const APP_MOUNTED = 'APP_MOUNTED';
