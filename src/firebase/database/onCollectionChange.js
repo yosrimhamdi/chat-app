db0381106d393b02be6db0abe386e86bc2f57894
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-27 15:24:40 +0100

    changed a variable name.

--- src/firebase/database/onCollectionChange.js
+++ src/firebase/database/onCollectionChange.js
@@ -2,9 +2,9 @@ import { getDatabase, ref, onValue } from 'firebase/database';
 
 const onCollectionChange = (collectionName, actionCreator) => {
   const db = getDatabase();
-  const channelsRef = ref(db, collectionName);
+  const collectionRef = ref(db, collectionName);
 
-  onValue(channelsRef, snapshot => {
+  onValue(collectionRef, snapshot => {
     const documents = Object.values(snapshot.val() || []);
 
     actionCreator(documents);
