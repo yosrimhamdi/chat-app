3f66a7ad0a9a24594ea6c5dfd153ee3f066f1439
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-29 20:41:44 +0100

    Move fetching/listening the channel creation up in the component hirerachy.

--- src/components/SidePanel/PublicChannels/UnStarredPublicChannels.js
+++ src/components/SidePanel/PublicChannels/UnStarredPublicChannels.js
@@ -1,37 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Menu, Icon } from 'semantic-ui-react';
 
 import './Channel.scss';
 import CreateChannelModal from '../../Modals/CreateChannelModal/CreateChannelModal';
 import Channel from './Channel';
-import removeListener from '../../../firebase/database/removeListener';
-import onChildAdded from '../../../firebase/database/onChildAdded';
-import fetchChannels from '@actions/fetchChannels';
 import ModalContext from '../../Modals/ModalContext';
 import useModal from '../../Modals/useModal';
-import readData from '../../../firebase/database/readData';
-import fetchChannel from '@actions/fetchChannel';
 import filterChannels from './filterChannels';
 
-function UnStarredPublicChannels({
-  unStarredChannels,
-  fetchChannels,
-  fetchChannel,
-}) {
+function UnStarredPublicChannels({ unStarredChannels }) {
   const [isModalOpen, openModal, closeModal] = useModal();
-
-  useEffect(() => {
-    const handleOnChildAdded = snap => fetchChannel(snap.val());
-
-    onChildAdded('channels/', handleOnChildAdded);
-
-    return () => removeListener('channels/', handleOnChildAdded);
-  }, [onChildAdded]);
-
-  useEffect(() => {
-    readData('channels/', fetchChannels);
-  }, []);
 
   const renderedPublicChannels = unStarredChannels.map(channel => (
     <Channel key={channel.id} channel={channel} />
@@ -58,6 +37,4 @@ const mapStateToProps = ({ auth, users, channels }) => ({
   unStarredChannels: filterChannels(auth, users, channels.all, 'UnStarred'),
 });
 
-export default connect(mapStateToProps, { fetchChannels, fetchChannel })(
-  UnStarredPublicChannels,
-);
+export default connect(mapStateToProps)(UnStarredPublicChannels);
--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import UserPanel from './UserPanel';
 import { Menu } from 'semantic-ui-react';
 
 import PrivateChannels from './PrivateChannels/PrivateChannels';
 import UnStarredPublicChannels from './PublicChannels/UnStarredPublicChannels';
 import StarredPublicChannels from './PublicChannels/StarredPublicChannels';
+import removeListener from '../../firebase/database/removeListener';
+import onChildAdded from '../../firebase/database/onChildAdded';
+import readData from '../../firebase/database/readData';
+import fetchChannels from '@actions/fetchChannels';
+import fetchChannel from '@actions/fetchChannel';
 
-function SidePanel() {
+function SidePanel({ fetchChannels, fetchChannel }) {
+  useEffect(() => {
+    const handleOnChildAdded = snap => fetchChannel(snap.val());
+
+    onChildAdded('channels/', handleOnChildAdded);
+
+    return () => removeListener('channels/', handleOnChildAdded);
+  }, [onChildAdded]);
+
+  useEffect(() => {
+    readData('channels/', fetchChannels);
+  }, []);
+
   return (
     <Menu
       size="large"
@@ -23,4 +41,4 @@ function SidePanel() {
   );
 }
 
-export default SidePanel;
+export default connect(null, { fetchChannels, fetchChannel })(SidePanel);
