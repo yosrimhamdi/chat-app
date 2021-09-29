0174182bf539d5d46f0040300ee8d66d4d48bdb8
yosrimhamdi <yosrimhamdi@outlook.com>
2021-09-29 21:47:00 +0100

    Displaying channel information.

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { Segment, Icon, Header, Accordion, Image } from 'semantic-ui-react';
 
-function MetaPanel() {
-  return <div>MetaPanel</div>;
-}
+const MetaPanel = ({ selectedChannel }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-export default MetaPanel;
+  const onAccordionTitleClick = (e, titleProps) => {
+    setActiveIndex(titleProps.index);
+  };
+
+  const { createdBy, isPrivate, details, name } = selectedChannel;
+
+  if (isPrivate) {
+    return null;
+  }
+
+  return (
+    <Segment>
+      <Header as="h3" attached="top">
+        About # {name}
+      </Header>
+      <Accordion styled attached="true">
+        <Accordion.Title
+          active={activeIndex === 0}
+          index={0}
+          onClick={onAccordionTitleClick}
+        >
+          <Icon name="dropdown" />
+          <Icon name="info" />
+          Channel Details
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 0}>
+          {details}
+        </Accordion.Content>
+
+        <Accordion.Title
+          active={activeIndex === 1}
+          index={1}
+          onClick={onAccordionTitleClick}
+        >
+          <Icon name="dropdown" />
+          <Icon name="user circle" />
+          Top Posters
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 1}>
+          posters
+        </Accordion.Content>
+
+        <Accordion.Title
+          active={activeIndex === 2}
+          index={2}
+          onClick={onAccordionTitleClick}
+        >
+          <Icon name="dropdown" />
+          <Icon name="pencil alternate" />
+          Created By
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 2}>
+          <Header as="h4">
+            <Image circular src={createdBy.photoURL} />
+            {createdBy.displayName}
+          </Header>
+        </Accordion.Content>
+      </Accordion>
+    </Segment>
+  );
+};
+
+const mapStateToProps = state => ({
+  selectedChannel: state.channels.selectedChannel,
+});
+
+export default connect(mapStateToProps)(MetaPanel);
