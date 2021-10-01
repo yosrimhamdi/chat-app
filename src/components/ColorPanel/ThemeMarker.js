ffe2eb187c46e428ac7a66575d3e973b6c76c2b8
yosrimhamdi <yosrimhamdi@outlook.com>
2021-10-01 13:50:59 +0100

    Marking redux theme colors.

--- src/components/ColorPanel/ThemeMarker.js
+++ src/components/ColorPanel/ThemeMarker.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './ThemeMarker.scss';
 
-const ThemeMarker = () => {
+const ThemeMarker = ({ theme }) => {
+  const { primaryColor, secondaryColor } = theme;
+
   return (
     <div className="theme-marker">
-      <div className="theme-marker__primary-color"></div>
-      <div className="theme-marker__secondary-color"></div>
+      <div
+        style={{ backgroundColor: primaryColor }}
+        className="theme-marker__primary-color"
+      ></div>
+      <div
+        style={{ backgroundColor: secondaryColor }}
+        className="theme-marker__secondary-color"
+      ></div>
     </div>
   );
 };
 
-export default ThemeMarker;
+const mapStateToProps = ({ theme }) => ({ theme });
+
+export default connect(mapStateToProps)(ThemeMarker);
