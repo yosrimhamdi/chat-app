65f4fe9c378570b1c2cf9caa7f4a69d0e83751e9
yosrimhamdi <yosrimhamdi@outlook.com>
2021-10-01 17:58:14 +0100

    Rendering all liste themes.

--- src/components/ColorPanel/Themes.js
+++ src/components/ColorPanel/Themes.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Theme from './Theme';
 
-const Themes = () => {
-  return <Theme theme={{}} />;
+const Themes = ({ themes }) => {
+  const renderedThemes = themes.map(theme => (
+    <Theme key={theme.id} theme={theme} />
+  ));
+
+  return renderedThemes;
 };
 
-export default Themes;
+const mapStateToProps = ({ users, auth }) => {
+  const uid = auth.user.uid;
+
+  const user = users.find(user => uid == user.uid);
+
+  return { themes: Object.values(user.themes) };
+};
+
+export default connect(mapStateToProps)(Themes);
