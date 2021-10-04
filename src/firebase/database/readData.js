eb91e77baed90d7a50766e21aff48448ecd2a895
yosrimhamdi <yosrimhamdi@outlook.com>
2021-10-04 09:19:20 +0100

    Changed a variable name.

--- src/firebase/database/readData.js
+++ src/firebase/database/readData.js
@@ -2,11 +2,11 @@ import { getDatabase, ref, child, get } from 'firebase/database';
 
 import catcher from '../../catcher';
 
-const readData = async (path, creator) => {
+const readData = async (path, callback) => {
   const dbRef = ref(getDatabase());
   const snapshot = await get(child(dbRef, path));
 
-  creator(Object.values(snapshot.val() || {}));
+  callback(Object.values(snapshot.val() || {}));
 };
 
 export default catcher(readData);
