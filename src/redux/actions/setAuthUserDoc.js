0e522ec5e6f54df620ded9dc91e252b275fa6d33
yosrimhamdi <yosrimhamdi@outlook.com>
2021-10-06 18:43:56 +0100

    Fixed a bug while regisetring.

--- src/redux/actions/setAuthUserDoc.js
+++ src/redux/actions/setAuthUserDoc.js
@@ -1,8 +1,16 @@
 import { AUTH_USER_DOCUMENT } from '@types';
 
-const setAuthUserDoc = snap => ({
-  type: AUTH_USER_DOCUMENT,
-  payload: Object.values(snap.val())[0],
-});
+const setAuthUserDoc = snap => {
+  const doc = snap.val();
+
+  if (!doc) {
+    return { type: '' };
+  }
+
+  return {
+    type: AUTH_USER_DOCUMENT,
+    payload: Object.values(snap.val())[0],
+  };
+};
 
 export default setAuthUserDoc;
